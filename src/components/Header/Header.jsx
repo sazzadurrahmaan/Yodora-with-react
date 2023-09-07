96312f6ae0e279650ae477d76e35533203d9b8d3
sazzadurrahman-cse <sazzadrn17@gmail.com>
2023-09-07 15:53:40 +0600

    completed navbar section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logoSvg from '../../assets/logo.svg'
 import matrixBar from '../../assets/menu-3.png'
 import './Header.css';
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+
+    const toggleMenu = () => {
+
+        setIsMenuOpen(!isMenuOpen);
+    };
     return (
         <nav className='flex items-center justify-between mx-10 my-5 '>
-            <div className="flex items-center p-5 md:text-2xl" id="left-nav">
+            <div className="flex items-center p-5 text-xl" id="left-nav">
                 <img src={logoSvg} alt="Logo" />
-                <div id="list" className='hidden md:block ml-4 space-x-4 '>
+                <div
+                    id="list"
+                    className={`md:block ml-4 space-x-4  ${isMenuOpen ? 'block flex flex-col text-center' : 'hidden'
+                        } md:flex`}
+                    style={{ left: 0, zIndex: 1 }}
+                >
                     <a href="">Explore</a>
                     <a href="">Company</a>
                     <a href="">Support</a>
                 </div>
             </div>
-            <div className="md:text-2xl">
+            <div>
                 <div id="desktop" className='hidden md:block'>
-                    <a className='mr-8'>Become a member</a>
+                    <a className='mr-8 text-xl'>Become a member</a>
                     <button className='bg-slate-800 text-white px-8 py-3 rounded-full'> Sign in</button>
                 </div>
-                <div className="menu-bar  md:hidden">
+                <div className=" md:hidden" onClick={toggleMenu} id="menu-bar">
                     <img src={matrixBar} alt="menubar" />
                 </div>
             </div>
